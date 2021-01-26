9b3bbd659323654aec65f46d2a96a2c6dbb23bdd
Evan Patrick <evandp1999@gmail.com>
2021-01-26 16:30:32 -0500

    ignoring illegal bits

--- simplebgc-derive/src/lib.rs
+++ simplebgc-derive/src/lib.rs
@@ -246,8 +246,7 @@ fn get_parser_for_field(info: &FieldInfo) -> Option<TokenStream2> {
             };
 
             Some(quote_spanned! {span=>
-                let #var = BitFlags::from_bits(_b.#get_value())
-                    .or(Err(PayloadParseError::InvalidFlags { name: #name.into() }))?;
+                let #var = BitFlags::from_bits_truncate(_b.#get_value());
             })
         }
         FieldKind::Enum { repr } => {
